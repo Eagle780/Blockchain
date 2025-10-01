bfa363a3730d027e6b2c514f58b6c3b6b0035111
Egle <egle.gurklyte@mif.stud.vu.lt>
2025-10-01 15:41:11 +0300

    teksto skaitymas is failo ir komandines eilutes

--- hash.cpp
+++ hash.cpp
@@ -1,13 +1,40 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
+void skaitytiFaila(string failas);
+
 int main(int argc, char **argv)
 {
-    for (int i = 0; i < argc; ++i)
+    if (argc == 2)
     {
-        cout << "argv[" << i << "]: " << argv[i] << endl;
+        string failas = argv[1];
+        skaitytiFaila(failas);
+    }
+    else if (argc > 3)
+    {
+        cout << "bloga ivestis" << endl;
+    }
+    else if (argc == 1)
+    {
+        string a;
+        cout << "iveskite teksta, kuri norite uzsifruoti:" << endl;
+        getline(cin, a);
+        cout << a << endl;
     }
     return 0;
+}
+
+void skaitytiFaila(string failas)
+{
+    ifstream fd(failas);
+    string eilute;
+    while (getline(fd, eilute))
+    {
+        cout << eilute << " ";
+    }
+    cout << endl;
+    fd.close();
 }

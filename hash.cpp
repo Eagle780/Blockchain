b693ca9185c41446f6532396e29dd88e0561d95b
Egle <egle.gurklyte@mif.stud.vu.lt>
2025-10-01 15:00:48 +0300

    kintamuju skaitymas is komandines eilutes

--- hash.cpp
+++ hash.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
-int main()
+int main(int argc, char **argv)
 {
-
+    for (int i = 0; i < argc; ++i)
+    {
+        cout << "argv[" << i << "]: " << argv[i] << endl;
+    }
     return 0;
 }

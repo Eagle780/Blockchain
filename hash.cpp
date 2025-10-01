d007f47d06f58c6b45735218bd5367b2d7232c05
Egle <egle.gurklyte@mif.stud.vu.lt>
2025-10-01 22:15:16 +0300

    is ivesties sudarytos 8 dalys po 8 skaicius

--- hash.cpp
+++ hash.cpp
@@ -9,6 +9,7 @@ using namespace std;
 string skaitytiFaila(string failas);
 string konvertuotiASCII(string tekstas);
 vector<string> paversti64(string tekstasASCII);
+int sudeti(string pirmas, string antras);
 
 int main(int argc, char **argv)
 {
@@ -41,31 +42,11 @@ int main(int argc, char **argv)
     cout << decimal << "\n";
     */
 
-    if (tekstasASCII.length() < 64)
-    {
-        cout << "trumpesnis: " << tekstasASCII.length() << endl;
-        int ilgis = tekstasASCII.length();
-        for (int i = 0; i < (64 - ilgis); i++)
-            tekstasASCII.append("0");
-    }
-    else if (tekstasASCII.length() > 64)
-    {
-        cout << "ilgesnis" << endl;
-        vector<string> data = paversti64(tekstasASCII);
-        for (int i = 0; i < data.size(); i++)
-        {
-            cout << data[i] << " " << data[i].length() << endl;
-        }
-        tekstasASCII = to_string(stoi(data[0]) + stoi(data[1]));
-        cout << stoi(data[0]) << endl;
-        cout << stoi(data[1]) << endl;
-        cout << tekstasASCII << endl;
-    }
-    else if (tekstasASCII.length() == 64)
-    {
-        cout << "lygu" << endl;
-    }
-    cout << "teksto ascii ilgis: " << tekstasASCII.length() << endl;
+    cout << tekstasASCII.length() << endl;
+
+    vector<string> dalys = paversti64(tekstasASCII);
+
+    cout << "daliu skaicius: " << dalys.size() << endl;
     return 0;
 }
 
@@ -110,9 +91,32 @@ vector<string> paversti64(string tekstasASCII)
         dalys.push_back(tekstasASCII.substr(i * 8, 8));
     }
 
+    if (dalys.size() > 8)
+    {
+        int skir = dalys.size() - 8;
+        for (int i = 0; i < skir; i++)
+        {
+            int sum = sudeti(dalys[i], dalys[i + 8]);
+            string naujas = to_string(sum);
+            dalys[i] = naujas;
+        }
+        for (int i = 0; i < skir; i++)
+            dalys.pop_back();
+    }
+
     for (int i = 0; i < dalys.size(); i++)
     {
         cout << dalys[i] << endl;
     }
+
     return dalys;
+}
+
+int sudeti(string pirmas, string antras)
+{
+    int sum = stoi(pirmas) + stoi(antras);
+    if (sum / 100000000 >= 1)
+        sum = sum % 100000000;
+
+    return sum;
 }
